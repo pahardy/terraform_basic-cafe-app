f1041521fad6227a0d8098f3ff298c121dade03e
Patrick Hardy <42849961+pahardy@users.noreply.github.com>
2024-01-15 13:06:15 -0500

    Update instance.tf to add dynamically generated security group for application access on appropriate port

--- instance.tf
+++ instance.tf
@@ -1,9 +1,30 @@
+resource "aws_security_group" "cafe-demo-SG-http" {
+  name = "Allow-HTTP-Cafe-Demo-SG"
+  description = "Allows HTTP traffic to the EC2 instance hosting the demo cafe app"
+
+  ingress {
+    description = "Allowed ingress traffic, port 80"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allowing all egress traffic from SG"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 #Creating the AWS instances to run the app
 resource "aws_instance" "cafe-app-instance" {
   ami           = var.ami
   instance_type = var.instance-type
   user_data = file("app1-install.sh")
-  security_groups = var.security-group
+  security_groups = [aws_security_group.cafe-demo-SG-http.name]
   for_each = toset(var.azs)
   tags = {
     "Name" = "${each.key}-BasicCafeApp-Terraform"
